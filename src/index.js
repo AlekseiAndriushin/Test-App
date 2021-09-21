b846e74a6a29536287c18638ac00079d96cd4293
alexey <andr200116alex@gmail.com>
2021-09-21 16:43:27 +0300

    FS-2 adding logic for adding element

--- src/index.js
+++ src/index.js
@@ -13,3 +13,58 @@ document
       );
     }
   });
+
+const submitForm = document.querySelector('.add');
+const addButton = document.querySelector('.add_component');
+const component = document.querySelector('.components');
+const list = document.querySelectorAll('.components li');
+
+let listLenght = list.lenght;
+
+const generateTempalate = (
+  companyName,
+  address,
+  email,
+  telephone
+) => {
+  const html = `<li>
+                  <label for="result_${listLenght}">
+                    <span class="check"></span>
+                    Название компании: ${companyName}
+                    <br/>
+                    Адрес: ${address}
+                    <br/>
+                    Email: ${email}
+                    <br/>
+                    Телефон: ${telephone}
+                    <br/>
+                  </label>
+                </li>`;
+  component.innerHTML += html;
+};
+
+function addBlock(e) {
+  e.preventDefault();
+  const companyName = submitForm.companyName.value.trim();
+  const address = submitForm.address.value.trim();
+  const email = submitForm.email.value.trim();
+  const telephone = submitForm.telephone.value.trim();
+  if (
+    companyName.length &&
+    address.length &&
+    email.length &&
+    telephone.length
+  ) {
+    listLenght = listLenght + 1;
+    generateTempalate(
+      companyName,
+      address,
+      email,
+      telephone
+    );
+    submitForm.reset();
+  }
+}
+
+submitForm.addEventListener('submit', addBlock);
+addButton.addEventListener('click', addBlock);
