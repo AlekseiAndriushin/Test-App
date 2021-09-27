53939792a7fd31e831eb1984afb8d0cddbb2e062
alexey <andr200116alex@gmail.com>
2021-09-28 00:00:52 +0300

    FS-2 function change getFollowers

--- src/queries/footerQueries.js
+++ src/queries/footerQueries.js
@@ -1,26 +1,29 @@
 const ulList = document.createElement('ul');
 ulList.classList.add('followersWrapper');
 export function getFollowers(repoFollowers) {
+  const renderFollower = (repoItem) => {
+    const listItem = document.createElement('li');
+
+    listItem.classList.add('followersItem');
+
+    const listItemTextNode = document.createTextNode(
+      repoItem.login
+    );
+
+    listItem.appendChild(listItemTextNode);
+
+    let image = document.createElement('img');
+
+    image.src = repoItem.avatar_url;
+    image.alt = repoItem.login;
+    image.classList.add('footer_image');
+
+    ulList.appendChild(listItem).appendChild(image);
+    document.querySelector('#footer').appendChild(ulList);
+  };
   if (repoFollowers) {
     repoFollowers.forEach((repoItem) => {
-      const listItem = document.createElement('li');
-
-      listItem.classList.add('followersItem');
-
-      const listItemTextNode = document.createTextNode(
-        repoItem.login
-      );
-
-      listItem.appendChild(listItemTextNode);
-
-      let image = document.createElement('img');
-
-      image.src = repoItem.avatar_url;
-      image.alt = repoItem.login;
-      image.width = 50;
-
-      ulList.appendChild(listItem).appendChild(image);
-      document.querySelector('#footer').appendChild(ulList);
+      renderFollower(repoItem);
     });
   } else {
     return;
