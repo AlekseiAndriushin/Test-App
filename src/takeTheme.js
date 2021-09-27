5dbd7b1480942686e08a5c86009dbad4fd042e8f
alexey <andr200116alex@gmail.com>
2021-09-27 23:48:21 +0300

    FS-2 adding takeTheme check

--- src/takeTheme.js
+++ src/takeTheme.js
@@ -1,14 +1,20 @@
-export const takeTheme = () =>
-  document
-    .querySelector('.changeTheme')
-    .addEventListener('change', (event) => {
-      if (event.target.nodeName === 'INPUT') {
-        document.documentElement.classList.remove(
-          'red',
-          'blue'
-        );
-        document.documentElement.classList.add(
-          event.target.value
-        );
-      }
-    });
+const themeRadios =
+  document.querySelectorAll('.themeRadios');
+const wrapThemeRadios =
+  document.querySelector('.changeTheme');
+export const takeTheme = () => {
+  const changeTheme = (event) => {
+    if (themeRadios.length) {
+      document.documentElement.classList.remove(
+        'red',
+        'blue'
+      );
+      document.documentElement.classList.add(
+        event.target.value
+      );
+    }
+  };
+  wrapThemeRadios.addEventListener('change', (event) => {
+    changeTheme(event);
+  });
+};
