06c68956bbf4074945fb423ccd8eff2bce08037c
alexey <andr200116alex@gmail.com>
2021-09-29 11:14:14 +0300

    FS-2 change to arrow functions

--- src/checkingElementHit.js
+++ src/checkingElementHit.js
@@ -3,24 +3,24 @@ const takeCard = document.querySelectorAll(
   '.components__card'
 );
 
-export function onClickCard(event) {
+export const onClickCard = (event) => {
   if (
     event.which === 3 &&
     event.target.nodeName === 'SPAN'
   ) {
     alert('Меню для карточки');
   }
-}
+};
 
 export const checkingElementHit = () => {
-  function onClickContentBlock(e) {
+  const onClickContentBlock = (e) => {
     if (
       (e.button === 2 && e.target.nodeName === 'SECTION') ||
       (e.button === 2 && e.target.nodeName === 'UL')
     ) {
       alert('Меню для списка компаний');
     }
-  }
+  };
 
   takeContentBlock.addEventListener(
     'mousedown',
--- src/outputSelectedCompanies.js
+++ src/outputSelectedCompanies.js
@@ -3,18 +3,18 @@ const findButton = document.querySelector(
 );
 
 export const outputSelectedCompanies = () => {
-  function findList() {
+  const findList = () => {
     const selectedCards = document.querySelectorAll(
       '.components__card__selected '
     );
     if (selectedCards.length) {
       console.clear();
-      selectedCards.forEach(function (child) {
+      selectedCards.forEach((child) => {
         console.log(child.firstChild.textContent);
       });
     } else {
       console.log('тут пусто');
     }
-  }
+  };
   findButton.addEventListener('click', findList);
 };
