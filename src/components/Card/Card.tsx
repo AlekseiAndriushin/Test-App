ceef43fc041e256d589301558c873ff258ac4b3a
alexey <andr200116alex@gmail.com>
2021-10-01 14:10:10 +0300

    FS-3 adding functionality in card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 interface IProps {
 	index: number;
+	text: string;
 }
 
-const Card = ({ index }: Readonly<IProps>): React.ReactElement => {
+const Card = ({ index, text = "" }: Readonly<IProps>): React.ReactElement => {
+	const [isActive, setIsActive] = useState<boolean>(false)
+
+	const toggleClass = () => {
+		setIsActive(!isActive)
+	}
+
 	return (
-		<li className="components__card" id={`${index}`} >
-			<span>Название компании:</span>
+		<li
+			className=
+			{isActive
+				? "components__card__selected"
+				: "components__card"
+			}
+			id={`${index}`}
+			onClick={toggleClass}>
+			<span>Название компании:{text}</span>
 			<span>Адрес:</span>
 			<span>Email:</span>
 			<span>Телефон:</span>
--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -7,7 +7,7 @@ export const Content = () => {
 			<ul className="components" id="components">
 				{[1, 2, 3, 4, 5, 6, 7, 8].map(index => {
 					return (
-						<Card index={index} key={index} />
+						<Card index={index} key={index} text={''} />
 					)
 				})}
 			</ul>
