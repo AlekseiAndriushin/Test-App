9a33fa84925638c9be659b22772509b95f9ea1a3
alexey <andr200116alex@gmail.com>
2021-10-01 09:30:18 +0300

    TS-3 adding menu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Header } from './components/Header/Header'
 import { Content } from './components/Content/Content'
+import Menu from './components/Menu/Menu'
 const App = () => {
 	return (
 		<>
 			<Header />
 			<Content />
+			<Menu />
 		</>
 	)
 }
