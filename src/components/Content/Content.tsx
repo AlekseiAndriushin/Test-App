053cd5f775e659b8fa64880abf41fa501fa83230
alexey <andr200116alex@gmail.com>
2021-10-01 10:46:27 +0300

    Fs-3 adding cards into content component

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Card from '../Card/Card'
 
 export const Content = () => {
 	return (
 		<div className="content">
-			content
+			<ul className="components" id="components">
+				{[1, 2, 3, 4, 5, 6, 7, 8].map(index => {
+					return (
+						<Card index={index} key={index} />
+					)
+				})}
+			</ul>
 		</div>
 	)
 }
