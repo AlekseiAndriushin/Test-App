d885890281de1cba4daa62715e4a2fb67015ea52
alexey <andr200116alex@gmail.com>
2021-10-01 09:37:30 +0300

    FS-3 adding form component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Header } from './components/Header/Header'
 import { Content } from './components/Content/Content'
 import Menu from './components/Menu/Menu'
+import Form from './components/Form/Form'
 const App = () => {
 	return (
 		<>
 			<Header />
 			<Content />
 			<Menu />
+			<Form />
 		</>
 	)
 }
---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Form = () => {
+	return (
+		<div className="form">
+			форма
+		</div>
+	)
+}
+
+export default Form
