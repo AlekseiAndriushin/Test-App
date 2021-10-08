0738931fe653e69f7e1f0ff09746165568d9cef3
alexey <andr200116alex@gmail.com>
2021-10-08 08:36:47 +0300

    FS-3 removing unneeded container

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
-import { Container } from './components/Container/Container'
-
+import { ChangeButtons } from './components/ChangeButtons/ChangeButtons'
+import { Content } from './components/Content/Content'
+import { FooterContainer } from './components/Footer/FooterContainer'
+import { FormContainer } from './components/Form/FormContainer'
+import { HeaderContainer } from './components/Header/HeaderContainer'
+import { MenuContainer } from './components/Menu/MenuContainer'
+import { useTheme } from './contexts/Theme/Theme.context'
+import './App.scss'
 export const App = () => {
+	const { theme } = useTheme()
+
 	return (
-		<Container />
+		<div className="container" style={{ ...theme as React.CSSProperties }}>
+			<HeaderContainer />
+			<Content />
+			<MenuContainer />
+			<FormContainer />
+			<ChangeButtons />
+			<FooterContainer />
+		</div>
 	)
 }
