7e31176fc7710c4687d4903377f1c3411197c9a0
alexey <andr200116alex@gmail.com>
2021-10-08 15:26:01 +0300

    FS-3 adding alert to component

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,15 +1,17 @@
-import React, { SyntheticEvent } from 'react'
+import React, { SyntheticEvent, useCallback } from 'react'
 import { ICard } from '../../features/Slices/cards/cardsSlice';
 import Card from '../Card/Card'
 import './Content.scss'
 
 interface IContentProps {
 	cards: ICard[];
-	onContextMenu: (e: SyntheticEvent) => void;
 }
 
-export const Content: React.FC<IContentProps> = ({ cards, onContextMenu }) => {
-
+export const Content: React.FC<IContentProps> = ({ cards }) => {
+	const onContextMenu = useCallback((event: React.MouseEvent) => {
+		event.preventDefault();
+		alert('Меню для списка компаний');
+	}, []);
 	return (
 		<section className="content" onContextMenu={onContextMenu}>
 			<ul className="components" >
