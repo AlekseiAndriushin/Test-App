259c0d503374a7b9cd62d7737b8838a0a94c135c
alexey <andr200116alex@gmail.com>
2021-10-18 15:19:34 +0300

    TS-4 fixing AboutPage

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -4,9 +4,12 @@ import './AboutPage.scss';
 
 export const AboutPage = () => (
   <div className='aboutPage'>
+    
     <div className='aboutPage__text'>привет!</div>
+    
     <Link to='/' className='aboutPage__linkText'>
       Нажми на меня чтобы вернуться на страницу с карточками
     </Link>
+    
   </div>
 );
