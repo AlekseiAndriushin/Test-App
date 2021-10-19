e3e2fe2fb99e6fde0c9a2b4b60317d53aa76944b
alexey <andr200116alex@gmail.com>
2021-10-19 09:17:00 +0300

    TS-4 fixing router

--- src/router/UnAuthRoute.tsx
+++ src/router/UnAuthRoute.tsx
@@ -1,20 +1,17 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import { publicRoutes, RouteNames } from '.';
+import { LoginPage } from '../pages';
+
+export enum RouteNames {
+	LOGIN = '/login',
+}
 
 export const UnAuthRoute = () => {
 	
 	return (
 		<Switch>
-			{publicRoutes.map((route) => (
-				<Route
-					path={route.path}
-					exact={route.exact}
-					component={route.component}
-					key={route.path}
-				/>
-			))}
-			<Redirect to={RouteNames.LOGIN}/>
+			<Route path={RouteNames.LOGIN} exact component={LoginPage} />
+			<Redirect to={RouteNames.LOGIN} />
 		</Switch>
 	)
 }
