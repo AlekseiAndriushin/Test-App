5f00ae2b0198061033ff108e00f8ec4197c4bf1e
 Алексей Андрюшин <aleksejandryushin@MacBook-Air-Aleksej.local>
2022-03-25 16:48:30 +0300

    Update fetchFollowers

--- src/store/queries/followers/fetchFollowers.ts
+++ src/store/queries/followers/fetchFollowers.ts
@@ -1,7 +1,13 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import NetworkService from '../../../API/NetworkService';
+import { GithubAPI, IGetFollowers } from '../../../api/githubApi';
+
+const prefix = 'github/';
+const getFollowersPrefix = `${prefix}fetchFollowers`;
 
 export const fetchFollowers = createAsyncThunk(
-  'followers/fetch',
-  NetworkService.getFollowers
+  getFollowersPrefix,
+	  async (data: IGetFollowers) => {
+    const response = await GithubAPI.getFollowers(data);
+    return response.data;
+  }
 );
